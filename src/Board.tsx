652df424df3becb60b78e45989b3697d18629fa8
Igor Serebryany <igor47@moomers.org>
2022-12-25 19:42:56 -0800

    render pieces on the board

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,64 +1,110 @@
+import { ReactNode } from 'react'
+
+import { Pawn, Rook, Knight, Bishop, Queen, King } from './Pieces'
 
 class Piece {
-  white: boolean | null = null
-  piece: 'p' | 'r' | 'k' | 'b' | 'q' | 'K' | null = null
+  white: boolean
+  piece: 'p' | 'r' | 'k' | 'b' | 'q' | 'K'
+
+  constructor(white: boolean, piece: Piece['piece']) {
+    this.white = white
+    this.piece = piece
+  }
+}
+
+class Square {
+  idx: number
+  white: boolean = true
+  piece: Piece | null = null
   selected: boolean = false
+
+  constructor (idx: number) {
+    this.idx = idx
+  }
 }
 
 type SquareP = {
   idx: number,
-  state: Piece[],
+  state: Square[],
 }
 
 function initialBoard() {
-  const pieces = Array(64).fill(null).map(() => new Piece())
-  for (let i = 0; i < pieces.length; i++) {
+  const squares = Array(64).fill(null).map((_v, idx) => new Square(idx))
+  for (let i = 0; i < squares.length; i++) {
     const row = i >> 3
     const col = i - (row << 3)
 
-    if (row >= 6) pieces[i].white = false
-    if (row <= 1) pieces[i].white = true
+    if (row % 2 === 0) {
+      squares[i].white = col % 2 === 0 ? false : true
+    } else {
+      squares[i].white = col % 2 === 0 ? true : false
+    }
 
-    if ([1,6].includes(row)) pieces[i].piece = 'p'
+    let piece: Piece['piece'] | null = null
+    const pieceWhite = row >= 6 ? false : true
+
+    if ([1,6].includes(row)) piece = 'p'
     if ([0,7].includes(row)) {
       if ([0, 7].includes(col)) {
-        pieces[i].piece = 'r'
+        piece = 'r'
       } else if ([1, 6].includes(col)) {
-        pieces[i].piece = 'k'
+        piece = 'k'
       } else if ([2, 5].includes(col)) {
-        pieces[i].piece = 'b'
+        piece = 'b'
       } else if (col === 3) {
-        pieces[i].piece = 'q'
+        piece = 'q'
       } else {
-        pieces[i].piece = 'K'
+        piece = 'K'
       }
+    }
+
+    if (piece !== null) {
+      squares[i].piece = new Piece(pieceWhite, piece)
     }
   }
 
-  return pieces
+  return squares
 }
 
-function Square({ idx, state }: SquareP) {
-  const bg = (idx >> 3) % 2 === 0 ? (
-    idx % 2 === 0 ? 'black': 'white') : ( idx % 2 === 0 ? 'white' : 'black' )
+function pieceEl(square: Square): ReactNode {
+  const piece = square.piece
+  if (piece === null)
+    return (<svg width="64px" height="64px" />)
+
+  const El =  {
+    'p': Pawn,
+    'r': Rook,
+    'k': Knight,
+    'b': Bishop,
+    'q': Queen,
+    'K': King,
+  }[piece.piece]
+
+  return <El size={ 64 } white={ piece.white } onWhite={ square.white } selected={ square.selected } />
+}
+
+function SquareEl({ idx, state }: SquareP) {
+  const square = state[idx]
 
   const style = {
-    backgroundColor: bg,
-    color: bg === 'black' ? 'white' : 'black',
-    minWidth: 128,
-    height: 128,
+    backgroundColor: square.white ? 'white' : 'black',
+    padding: '16px',
   }
 
+  const piece = pieceEl(state[idx])
+
   return (
-    <div style={ style }>{ state[idx].piece }</div>
+    <div style={ style }>
+      { piece }
+    </div>
   )
 }
 
 function Board() {
   const state = initialBoard()
 
-  const squares = Array(64).fill(null).map((_v, idx) =>
-    <Square key={idx} idx={idx} state={ state }/>
+  const squares = state.map((_wq, idx) =>
+    <SquareEl key={idx} idx={idx} state={ state }/>
   )
 
   return (
